03e7aede65f06efba9be4fe673a66d091c0b754c
LightningDev1 <59872288+LightningDev1@users.noreply.github.com>
2023-08-07 18:45:47 +0200

    make GuildID a json.Number

--- structs.go
+++ structs.go
@@ -2137,7 +2137,7 @@ type UserGuildSettings struct {
 	MobilePush           bool                                `json:"mobile_push"`
 	MessageNotifications int                                 `json:"message_notifications"`
 	HideMutedChannels    bool                                `json:"hide_muted_channels"`
-	GuildID              string                              `json:"guild_id"`
+	GuildID              json.Number                         `json:"guild_id"`
 	ChannelOverrides     []*UserGuildSettingsChannelOverride `json:"channel_overrides"`
 }
 
