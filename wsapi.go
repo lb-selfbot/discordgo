68677b26bfefd9db94a751f5ebe50331eb577f72
Lightning <lightning@lightning-bot.com>
2024-01-01 12:56:34 +0100

    fix guild subs

--- wsapi.go
+++ wsapi.go
@@ -217,7 +217,7 @@ func (s *Session) subscribeGuilds(wsConn *websocket.Conn, listening <-chan inter
 	first := true
 
 	for _, guild := range s.State.Guilds {
-		if !(guild.Large || guild.Unavailable) {
+		if guild.MemberCount < 65000 {
 			s.log(LogInformational, "skipping guild subscription for %s", guild.ID)
 			continue
 		}
